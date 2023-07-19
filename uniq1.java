2c81f87f4af1856983d26d116b3bb5f37d476e30
war-daredevil <ankurwr7@gmail.com>
2023-07-20 00:09:35 +0530

    unique problem

---
+++ uniq1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class uniq1 {
+	public static int kthSmallest(Integer[] arr, int K)
+	{
+		Arrays.sort(arr);
+
+		return arr[K - 1];
+	}
+
+	public static void main(String[] args)
+	{
+		Integer arr[] = new Integer[] { 12, 3, 5, 7, 19 };
+		int K = 2;
+
+		System.out.print("K'th smallest element is "
+						+ kthSmallest(arr, K));
+	}
+}
